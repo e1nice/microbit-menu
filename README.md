3994d04f53ec4e9b159a5f5d06d0b6f5e933cf12
Iwan Nijs - de Boer <iwan@nicethoughts.eu>
2023-04-16 13:53:24 +0200

    Added documentation.

---
+++ README.md
@@ -0,0 +1,5 @@
+# MicroPython menu for micro:bit
+
+A menu to select which code to run on a micro:bit using the display and buttons.
+
+A working example can be found in the example folder.
--- example/main.py
+++ example/main.py
@@ -1,3 +1,13 @@
+"""Example how to use the menu on a micro:bit
+
+* Upload the files menu.py, emotions.py, ok_or_not_ok.py and main.py to the micro:bit.
+* Start the code by pressing the reset button.
+* Use button A to select a menu option and button B to start this option.
+* Selecting the X option will exit the menu program.
+* The other two options repeat a sequence on the display, where button B exits to the menu.
+
+"""
+
 import microbit as mb
 from menu import Menu
 import ok_or_not_ok
